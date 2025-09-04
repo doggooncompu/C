b0bf478336a46a23ca8f2f4f5597ac33e6b286eb
doggooncompu <Ethan.Zhang28@ycdsbk12.ca>
2025-09-04 16:08:02 +0000

    build simulation

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+
+// define struct object -- used for creating two objects to run in the simulation 
+typedef struct {
+    double mass;
+    double velocityx;
+    double velocityy;
+    double accelx;
+    double accely;
+    int x;
+    int y;
+} OBJECT;
+
 int main(){
-    printf("testing");
+    int g = 0.1; //gravitational constant
+
+    //---------- INITIALIZE OBJECT1 --------------//
+    OBJECT matter1; 
+    matter1.mass = 1;
+    matter1.velocityx  = 0;
+    matter1.velocityy = 0;
+    matter1.x = 0;
+    matter1.y = 5;
+    
 }
